db979113d72dae29f2de3f75cfccc8ae4fa216cb
Michael Kania <dynamike@truss.works>
2018-09-07 16:45:57 -0700

    add prefix logging for NLBs

--- variables.tf
+++ variables.tf
@@ -38,6 +38,12 @@ variable "alb_logs_prefix" {
   type        = "string"
 }
 
+variable "nlb_logs_prefix" {
+  description = "S3 prefix for NLB logs."
+  default     = "nlb"
+  type        = "string"
+}
+
 variable "cloudtrail_logs_prefix" {
   description = "S3 prefix for CloudTrail logs."
   default     = "cloudtrail"
