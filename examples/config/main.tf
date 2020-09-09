4d3b9d83f31987012a0fbe67496afc986a8f0807
Armando Martinez <armando.martinez@truss.works>
2020-09-09 15:15:53 -0700

    Got Config test to work finally

--- examples/config/main.tf
+++ examples/config/main.tf
@@ -9,12 +9,12 @@ module "aws_logs" {
   force_destroy = var.force_destroy
 }
 
+
 module "config" {
   source  = "trussworks/config/aws"
-  version = "~> 3"
+  version = "~> 4"
 
-  config_name          = var.test_name
-  config_logs_bucket   = module.aws_logs.aws_logs_bucket
-  config_logs_prefix   = var.config_logs_prefix
-  config_sns_topic_arn = "bandana"
+  config_name        = var.test_name
+  config_logs_bucket = module.aws_logs.aws_logs_bucket
+  config_logs_prefix = var.config_logs_prefix
 }
--- test/terraform_aws_logs_config_test.go
+++ test/terraform_aws_logs_config_test.go
@@ -1,6 +1,5 @@
 package test
 
-/*
 import (
 	"fmt"
 	"strings"
@@ -14,17 +13,20 @@ import (
 func TestTerraformAwsLogsConfig(t *testing.T) {
 	t.Parallel()
 
+	configName := fmt.Sprintf("aws-config-%s", strings.ToLower(random.UniqueId()))
+
 	testName := fmt.Sprintf("terratest-aws-logs-%s", strings.ToLower(random.UniqueId()))
 	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/config")
 	// AWS only supports one configuration recorder per region.
 	// Each test using aws-config will need to specify a different region.
-	awsRegion := "us-east-2"
+	awsRegion := "us-east-1"
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: tempTestFolder,
 		Vars: map[string]interface{}{
 			"region":             awsRegion,
 			"test_name":          testName,
+			"config_name":        configName,
 			"force_destroy":      true,
 			"config_logs_prefix": testName,
 		},
@@ -40,17 +42,20 @@ func TestTerraformAwsLogsConfig(t *testing.T) {
 func TestTerraformAwsLogsConfigRootPrefix(t *testing.T) {
 	t.Parallel()
 
+	configName := fmt.Sprintf("aws-config-%s", strings.ToLower(random.UniqueId()))
+
 	testName := fmt.Sprintf("terratest-aws-logs-%s", strings.ToLower(random.UniqueId()))
 	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/config")
 	// AWS only supports one configuration recorder per region.
 	// Each test using aws-config will need to specify a different region.
-	awsRegion := "us-east-1"
+	awsRegion := "us-east-2"
 
 	terraformOptions := &terraform.Options{
 		TerraformDir: tempTestFolder,
 		Vars: map[string]interface{}{
 			"region":             awsRegion,
 			"test_name":          testName,
+			"config_name":        configName,
 			"force_destroy":      true,
 			"config_logs_prefix": "",
 		},
@@ -62,4 +67,3 @@ func TestTerraformAwsLogsConfigRootPrefix(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 }
-*/
